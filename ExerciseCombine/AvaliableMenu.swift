216a34ca464f492998ae20e77dfe88f182c169ba
athoya <athoyaa@gmail.com>
2022-08-29 11:34:07 +0700

    initial commit

---
+++ ExerciseCombine/AvaliableMenu.swift
@@ -0,0 +1,12 @@
+//
+//  AvaliableMenu.swift
+//  ExerciseCombine
+//
+//  Created by Athoya on 29/08/22.
+//
+
+import Foundation
+
+struct AvaliableMenu {
+    let list: [String] = ["Doughnout", "Spagetti", "Cupcake", "Ice cream", "Noodles", "Apple pie", "Shortcake", "Sushi"]
+}
---
+++ ExerciseCombine/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  ExerciseCombine
+//
+//  Created by Athoya on 29/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var menus: [String] = []
+    var filteredMenus: [String] = []
+    
+    @IBOutlet weak var menuTableView: UITableView!
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        menus = AvaliableMenu().list
+        filteredMenus = menus
+        
+        menuTableView.delegate = self
+        menuTableView.dataSource = self
+    }
+    
+    @IBAction func searchOnValueChanged(_ sender: Any) {
+        print(searchTextField.text)
+    }
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return filteredMenus.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "menuCell", for: indexPath)
+        cell.textLabel?.text = filteredMenus[indexPath.row]
+        return cell
+    }
+    
+    
+}
