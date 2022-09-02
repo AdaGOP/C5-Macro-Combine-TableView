a59aa9a83b481d604a7396eb087221c4f1f557d7
athoya <athoyaa@gmail.com>
2022-09-02 10:57:12 +0700

    add subject, add cancellabe, add combine operator

--- ExerciseCombine/ViewController.swift
+++ ExerciseCombine/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Combine
 
 class ViewController: UIViewController {
     
@@ -15,6 +16,12 @@ class ViewController: UIViewController {
     @IBOutlet weak var menuTableView: UITableView!
     @IBOutlet weak var searchTextField: UITextField!
     
+    // 1. Buat dulu kita variable subjectnya
+    var searchText: CurrentValueSubject<String,Never> = CurrentValueSubject("")
+    
+    // 2. Kita buat variable cancellabe, kalo misal subject ga dipake kita bisa buang dari memory
+    var subsciprions: Set<AnyCancellable> = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         menus = AvaliableMenu().list
@@ -22,10 +29,44 @@ class ViewController: UIViewController {
         
         menuTableView.delegate = self
         menuTableView.dataSource = self
+        
+        // 3. Kasih subscription dan behaviour kalo ada data berubah gimana
+        //        searchText.sink { value in
+        //            self.filteredMenus = self.menus.filter({ item in
+        //                item.lowercased().contains(self.searchTextField.text?.lowercased() ?? "")
+        //            })
+        //            self.menuTableView.reloadData()
+        //        }.store(in: &subsciprions)
+        
+        // 5. Coba operasi operasi data stream
+        searchText
+            .dropFirst()
+            .filter({ value in
+                value.count >= 3
+            })
+            .debounce(for: .milliseconds(1000), scheduler: RunLoop.main)
+            .sink { value in
+            self.filteredMenus = self.menus.filter({ item in
+                item.lowercased().contains(self.searchTextField.text?.lowercased() ?? "")
+            })
+            self.menuTableView.reloadData()
+        }.store(in: &subsciprions)
+        
+//        let textOnChange = NotificationCenter.default
+//            .publisher(for: UITextField.textDidChangeNotification, object: searchTextField)
+//            .sink { value in
+//                self.filteredMenus = self.menus.filter({ item in
+//                    item.lowercased().contains(self.searchTextField.text?.lowercased() ?? "")
+//                })
+//                self.menuTableView.reloadData()
+//        }.store(in: &subsciprions)
     }
     
     @IBAction func searchOnValueChanged(_ sender: Any) {
         print(searchTextField.text)
+        
+        // 4. Tambah data stream ke subject kita
+        searchText.send(searchTextField.text ?? "")
     }
 }
 
